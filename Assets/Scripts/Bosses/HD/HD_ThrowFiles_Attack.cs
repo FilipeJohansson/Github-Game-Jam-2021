5af335b55e70208d110134d0aac32168e56f6c41
Gabriel-M-Martins <gabrielmartinesco@gmail.com>
2021-11-05 10:20:14 -0300

    Minor tweaks to HD throw attack

--- Assets/Scripts/Bosses/HD/HD_ThrowFiles_Attack.cs
+++ Assets/Scripts/Bosses/HD/HD_ThrowFiles_Attack.cs
@@ -8,30 +8,42 @@ public class HD_ThrowFiles_Attack : MonoBehaviour
     Sprite[] _FilesSprites;
 
     [SerializeField]
-    float _travelSpeed, timeToDestroy;
-    float currentTimeToDestroy;
+    float _travelSpeed, _timeToDestroy;
+    float _currentTimeToDestroy;
 
     GameObject _player;
     Vector3 _target;
     bool _collided = false;
+    Sprite _chosenFileSprite;
 
     // Start is called before the first frame update
     void Awake()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
         _target = _player.transform.position;
-        currentTimeToDestroy = timeToDestroy;
-        GetComponent<SpriteRenderer>().sprite = _FilesSprites[Random.Range(0, _FilesSprites.Length)];
+
+        _chosenFileSprite = _FilesSprites[Random.Range(0, _FilesSprites.Length)];
+        GetComponent<SpriteRenderer>().sprite = _chosenFileSprite;
+        
+        _currentTimeToDestroy = _timeToDestroy;
     }
 
     void FixedUpdate()
     {
-        if (currentTimeToDestroy <= 0 || _collided)
+        if (_currentTimeToDestroy <= 0 || _collided) 
+        {
+            if (_chosenFileSprite.name != "fileBat.png")
+            {
+                //TODO: Acionar efeito no player (slow, dano reduzido ou algo assim sei la)
+            }
+
             Destroy(gameObject);
+        }
         else 
         {
+            // viaja ate o player
             transform.position = Vector3.Lerp(transform.position, _target, _travelSpeed * Time.deltaTime);
-            currentTimeToDestroy -= Time.deltaTime;
+            _currentTimeToDestroy -= Time.deltaTime;
         }
     }
 
