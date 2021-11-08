0e49e64beb7f0c7d1242531f1c5cd9ac19388c4e
Filipe Johansson <filipejohansson7@gmail.com>
2021-11-08 12:37:42 -0300

    PSU show shock attack area

--- Assets/Scripts/Bosses/PSU/PSU_Behaviour.cs
+++ Assets/Scripts/Bosses/PSU/PSU_Behaviour.cs
@@ -11,6 +11,14 @@ public class PSU_Behaviour : MonoBehaviour {
 
     [SerializeField]
     GameObject shockWave;
+    [SerializeField]
+    GameObject attackArea;
+    bool showAttackArea = false;
+
+    bool lookingToPlayer = true;
+
+    GameObject mainCamera;
+    public bool isFacing = true;
 
     // Start is called before the first frame update
     void Awake() {
@@ -23,16 +31,51 @@ public class PSU_Behaviour : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate() {
-        // Boss look at the player
-        transform.LookAt(player.transform);
+        if (lookingToPlayer) {
+            // Rotate towards player
+            transform.LookAt(player.transform);
+        }
 
         // Verify if can attack
         if (currentTimeToAttack <= 0) {
-            // Spawn the attack
-            Instantiate(shockWave, transform.position, transform.rotation);
-            // Reset timeToAttack
-            currentTimeToAttack = timeToAttack;
+            StartCoroutine(ShockAttack());
         } else
             currentTimeToAttack -= Time.deltaTime;
+
+        if(showAttackArea) {
+             StartCoroutine(ShowAttackArea());
+        }
+    }
+
+    IEnumerator ShockAttack() {
+        lookingToPlayer = false;
+        // Reset timeToAttack
+        currentTimeToAttack = timeToAttack;
+
+        showAttackArea = true;
+
+        yield return new WaitForSeconds(1f);
+
+        showAttackArea = false;
+
+        // Spawn the attack
+        Instantiate(shockWave, transform.position, transform.rotation);
+
+        yield return new WaitForSeconds(.5f);
+
+        lookingToPlayer = true;
+
+        yield return null;
+    }
+
+    IEnumerator ShowAttackArea() {
+        attackArea.SetActive(true);
+        attackArea.GetComponentInChildren<SpriteRenderer>().material.color = new Color(1, 1, 1, .7f);
+
+        yield return new WaitForSeconds(1.5f);
+
+        attackArea.SetActive(false);
+
+        yield return null;
     }
 }
